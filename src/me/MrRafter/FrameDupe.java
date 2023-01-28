31a4dadeffa80348f6ff31ee820187901f94165d
MrRafter <98182967+MrRafter@users.noreply.github.com>
2023-01-27 23:39:55 -0300

    Update FrameDupe.java

--- src/me/MrRafter/FrameDupe.java
+++ src/me/MrRafter/FrameDupe.java
@@ -4,7 +4,7 @@ import me.MrRafter.framedupe.Metrics;
 import me.MrRafter.framedupe.CommandCompleter;
 import me.MrRafter.framedupe.Commands;
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.EntityType;
@@ -27,7 +27,7 @@ public final class FrameDupe extends JavaPlugin {
         private void onFrameBreak(EntityDamageByEntityEvent event) {
             if (getConfig().getBoolean("FrameDupe.Enable") && event.getEntityType() == EntityType.ITEM_FRAME) {
                 int rng = (int)Math.round(Math.random() * 100);
-                if (rng < getConfig().getInt("FrameDupe.probability-percentage")) {
+                if (rng < getConfig().getInt("FrameDupe.Probability-percentage")) {
                     event.getEntity().getWorld().dropItemNaturally(event.getEntity().getLocation(), ((ItemFrame) event.getEntity()).getItem());
                 }
             }
@@ -50,65 +50,65 @@ public final class FrameDupe extends JavaPlugin {
     public void onEnable() {
         //Frame Dupe
         if (Bukkit.getVersion().contains("1.8")){
-            Bukkit.getLogger().info("Server on 1.8.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.8.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
         }
         if (Bukkit.getVersion().contains("1.9")){
-            Bukkit.getLogger().info("Server on 1.9.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.9.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
         }
         if (Bukkit.getVersion().contains("1.10")){
-            Bukkit.getLogger().info("Server on 1.10.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.10.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
         }
         if (Bukkit.getVersion().contains("1.11")){
-            Bukkit.getLogger().info("Server on 1.11.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.11.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
         }
         if (Bukkit.getVersion().contains("1.12")){
-            Bukkit.getLogger().info("Server on 1.12.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.12.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
         }
         if (Bukkit.getVersion().contains("1.13")){
-            Bukkit.getLogger().info("Server on 1.13.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.13.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
         }
         if (Bukkit.getVersion().contains("1.14")){
-            Bukkit.getLogger().info("Server on 1.14.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.14.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
         }
         if (Bukkit.getVersion().contains("1.15")){
-            Bukkit.getLogger().info("Server on 1.15.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.15.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
         }
         if (Bukkit.getVersion().contains("1.16")){
-            Bukkit.getLogger().info("Server on 1.16.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.16.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
         }
         if (Bukkit.getVersion().contains("1.17")){
-            Bukkit.getLogger().info("Server on 1.17.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.17.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
             getServer().getPluginManager().registerEvents(new FrameSpecific(), this);
         }
         if (Bukkit.getVersion().contains("1.18")){
-            Bukkit.getLogger().info("Server on 1.18.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.18.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
             getServer().getPluginManager().registerEvents(new FrameSpecific(), this);
         }
         if (Bukkit.getVersion().contains("1.19")){
-            Bukkit.getLogger().info("Server on 1.19.x applying the changes by version...");
-            Bukkit.getLogger().info("Changes by version applied correctly");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Server on 1.19.x applying the changes by version...");
+            Bukkit.getLogger().info(ChatColor.GREEN + "Changes by version applied correctly");
             getServer().getPluginManager().registerEvents(new FrameAll(), this);
             getServer().getPluginManager().registerEvents(new FrameSpecific(), this);
         }
@@ -120,6 +120,6 @@ public final class FrameDupe extends JavaPlugin {
         Metrics metrics = new Metrics(this, pluginId);
 
         saveDefaultConfig();
-        Bukkit.getLogger().info("FrameDupe by MrRafter loaded succesfully");
+        Bukkit.getLogger().info(ChatColor.GREEN + "FrameDupe by MrRafter loaded succesfully");
     }
 }
