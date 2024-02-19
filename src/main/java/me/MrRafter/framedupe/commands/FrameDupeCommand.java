99c87294f934f19bffa6fcf859edbca942eeda2d
xGinko <rachel.audrey@mail.de>
2024-02-19 13:14:55 +0100

    improve commands

--- src/main/java/me/MrRafter/framedupe/commands/FrameDupeCommand.java
+++ src/main/java/me/MrRafter/framedupe/commands/FrameDupeCommand.java
@@ -8,44 +8,45 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FrameDupeCommand implements TabCompleter, CommandExecutor {
 
-    private final List<SubCommand> subCommands = new ArrayList<>(2);
-    private final List<String> tabCompleter = new ArrayList<>(2);
+    private final List<SubCommand> subCommands;
+    private final List<String> tabCompleter;
 
     public FrameDupeCommand() {
-        subCommands.add(new ReloadSubCmd());
-        subCommands.add(new VersionSubCmd());
-        subCommands.forEach(subCommand -> tabCompleter.add(subCommand.getName()));
+        subCommands = Arrays.asList(new ReloadSubCmd(), new VersionSubCmd());
+        tabCompleter = subCommands.stream().map(SubCommand::getName).collect(Collectors.toList());
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        return args.length == 1 ? tabCompleter : null;
+        return args.length == 1 ? tabCompleter : Collections.emptyList();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length > 0) {
-            boolean cmdExists = false;
-            for (SubCommand subCommand : subCommands) {
-                if (args[0].equalsIgnoreCase(subCommand.getName())) {
-                    subCommand.perform(sender, args);
-                    cmdExists = true;
-                    break;
-                }
-            }
-            if (!cmdExists) showCommandOverviewTo(sender);
-        } else {
-            showCommandOverviewTo(sender);
+        if (args.length == 0) {
+            sendCommandOverview(sender);
+            return true;
         }
+
+        for (final SubCommand subCommand : subCommands) {
+            if (args[0].equalsIgnoreCase(subCommand.getName())) {
+                subCommand.perform(sender, args);
+                return true;
+            }
+        }
+
+        sendCommandOverview(sender);
         return true;
     }
 
-    private void showCommandOverviewTo(CommandSender sender) {
+    private void sendCommandOverview(CommandSender sender) {
         if (!sender.hasPermission("framedupe.cmd.*")) return;
         sender.sendMessage(ChatColor.GRAY+"-----------------------------------------------------");
         sender.sendMessage(ChatColor.WHITE+"FrameDupe Commands");
